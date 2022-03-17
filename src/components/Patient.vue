1617b5ebcf00e0902d8bd089f4df6970769eedce
jsandblom <js@ndblom.se>
2022-03-17 19:05:12 +0100

    Update Patient.vue

--- src/components/Patient.vue
+++ src/components/Patient.vue
@@ -1,3 +1,12 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+<Plats />
+<Id /> <!-- namn, pnr, GVd, GVnu, fvikt, vikt nu, procent upp eller ner, ålder -->
+<Socialt /> <!-- föräldranamn, syskon, problem -->
+<Problem /> <!-- andningsstörning, bili, kir, hjärt, annat, kryssrutor? -->
+<Andning /> <!-- maskin, inställningar -->
+<Nutrition /> <!-- sort, mängd, intervall, vätska, räknevätska -->
+<Läkemedel /> <!-- för inotropi också uträknat per kg -->
+<Aktuellt />
+</template>
